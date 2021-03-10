98eeb613d223af4c1adab69678bcc2cce96de61a
Morteza Ansarinia <ansarinia@me.com>
2021-03-10 23:08:25 +0100

    LFS-tracked tidy data
    Former-commit-id: 41c2ff1ae68a952bd10336d347b9eb47e3001f73
    Former-commit-id: 0d9d947289b2ea6a5f1eda89c954ba277d219585

--- python/cogtext/pubmed.py
+++ python/cogtext/pubmed.py
@@ -11,10 +11,10 @@ def search_and_store(term, output_file, db='pubmed', api_key=os.environ['NCBI_AP
   """Search for a term and store abstracts in a file
 
   Args:
-      term (str): [description]
-      output_file (Path): [description]
+      term (str): search query to look for in titles and abstracts.
+      output_file (Path): File path to store results.
       db (str, optional): NCBI database to search ('pubmed' or  'pmc'). Defaults to 'pubmed'.
-      api_key (string, optional): NCBI api key. Defaults to os.environ['NCBI_API_KEY'].
+      api_key (string, optional): NCBI api key. Defaults to `os.environ['NCBI_API_KEY']`.
 
   Returns:
       Does not return anything. Abstracts will be stored in the `output_file`.
@@ -25,30 +25,29 @@ def search_and_store(term, output_file, db='pubmed', api_key=os.environ['NCBI_AP
   search_query = f'({term}[TIAB])'
   url = f'{NLM_BASE_URL}/esearch.fcgi'
   params = {
-    'term': search_query.replace(' ','+'),
-    'usehistory': 'y',
-    'db': db,
-    'retmax': 0,
-    'reldate': 21 * 365,
-    'api_key': api_key
+      'term': search_query.replace(' ', '+'),
+      'usehistory': 'y',
+      'db': db,
+      'retmax': 0,
+      'reldate': 21 * 365,
+      'api_key': api_key
   }
 
-  response = requests.get(url,params=params)
+  response = requests.get(url, params=params)
   search_response = xmltodict.parse(response.text)
   results_count = search_response['eSearchResult']['Count']
 
-
-  print(f'Succesfully stored search results on history server. Now retriving {results_count} abstracts...')
+  print(f'{term}: succesfully stored {results_count} search hits on NCBI history server. Now retriving them...')
 
   # step 2: fetch abstracts
   url = f'{NLM_BASE_URL}/efetch.fcgi'
   params = {
-    'db': db,
-    'api_key': api_key,
-    'WebEnv': search_response['eSearchResult']['WebEnv'],
-    'query_key': search_response['eSearchResult']['QueryKey'],
-    'rettype': 'abstract',
-    'retmode': 'xml'
+      'db': db,
+      'api_key': api_key,
+      'WebEnv': search_response['eSearchResult']['WebEnv'],
+      'query_key': search_response['eSearchResult']['QueryKey'],
+      'rettype': 'abstract',
+      'retmode': 'xml'
   }
 
   response = requests.post(url, params)
@@ -57,4 +56,4 @@ def search_and_store(term, output_file, db='pubmed', api_key=os.environ['NCBI_AP
   Path(output_file).parent.mkdir(parents=True, exist_ok=True)
   with open(output_file, 'a') as f:
     f.write(response.text)
-  print(f'Succesfully stored results to {output_file}')
+  print(f'{term}: Succesfully stored search hits in {output_file}')
