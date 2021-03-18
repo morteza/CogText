8b5ad0bc29fa0f6e98a24f1c23bba9a98374080a
Morteza Ansarinia <ansarinia@me.com>
2021-03-18 11:13:24 +0100

    updated pubmed dataset
    Former-commit-id: b2a1f8f3f6de61722aa4b2a1782cd7adea815a33

--- python/cogtext/pubmed.py
+++ python/cogtext/pubmed.py
@@ -2,12 +2,13 @@ import os
 import requests
 import xmltodict
 from pathlib import Path
+from collections import OrderedDict 
 
 
 NCBI_EUTILS_BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
 
 
-def search_and_store(term, output_file, db='pubmed', api_key=os.environ['NCBI_API_KEY'], suffix=[]):
+def search_and_store(query, output_file, db='pubmed', api_key=os.environ['NCBI_API_KEY']):
   """Search for a term and store abstracts in a file
 
   Args:
@@ -15,7 +16,6 @@ def search_and_store(term, output_file, db='pubmed', api_key=os.environ['NCBI_AP
     output_file (Path): File path to store results.
     db (str, optional): NCBI database to search ('pubmed' or  'pmc'). Defaults to 'pubmed'.
     api_key (string, optional): NCBI api key. Defaults to `os.environ['NCBI_API_KEY']`.
-    suffix (list, optional): add optional suffix to the query, e.g., 'task', test', or 'survey'. Defaults to `[]`.
 
   Returns:
     Does not return anything. Abstracts will be stored in the `output_file`.
@@ -23,22 +23,11 @@ def search_and_store(term, output_file, db='pubmed', api_key=os.environ['NCBI_AP
 
   # step 1: create query and search
 
-  search_queries = []
-
-  if len(suffix) == 0:
-    search_queries.append(f'("{term}"[TIAB])')
-
-  for sfx in suffix:
-      search_queries.append(f'("{term} {sfx}"[TIAB])')
-
-  # build final search query
-  search_query = ' OR '.join(search_queries)  # .replace(' ', '+')
-
-  print(f'{term}: query={search_query}')
+  print(f'[PubMed] {query}')
 
   url = f'{NCBI_EUTILS_BASE_URL}/esearch.fcgi'
   params = {
-      'term': search_query,
+      'term': query,
       'usehistory': 'y',
       'db': db,
       'retmax': 0,
@@ -50,7 +39,7 @@ def search_and_store(term, output_file, db='pubmed', api_key=os.environ['NCBI_AP
   search_response = xmltodict.parse(response.text)
   results_count = search_response['eSearchResult']['Count']
 
-  print(f'{term}: succesfully stored {results_count} search hits on NCBI history server. Now retriving them...')
+  print(f'[PubMed] Succesfully stored {results_count} hits on NCBI history server.')
 
   # step 2: fetch abstracts
   url = f'{NCBI_EUTILS_BASE_URL}/efetch.fcgi'
@@ -67,6 +56,17 @@ def search_and_store(term, output_file, db='pubmed', api_key=os.environ['NCBI_AP
 
   # step 3: store abstracts
   Path(output_file).parent.mkdir(parents=True, exist_ok=True)
-  with open(output_file, 'a') as f:
+  with open(output_file, 'w') as f:
     f.write(response.text)
-  print(f'{term}: Succesfully stored search hits in {output_file}')
+  print(f'[PubMed] Succesfully stored hits in {output_file}.')
+
+
+def cleanup_abstract(abstract_text):
+    """PubMed returns abstract with semantic tags. This function cleans those tags and keep the text."""
+    select_content = lambda c: c if isinstance(c ,str) else c['#text'] if (c is not None and ('#text' in c)) else ''
+
+    if isinstance(abstract_text, list):
+        return ' '.join([select_content(a) for a in abstract_text])
+    elif isinstance(abstract_text, OrderedDict):
+        return select_content(abstract_text)
+    return abstract_text    # when the abstract is string
