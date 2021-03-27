47ecffa4c78b1b773b55f073f78f1e89a7a1ae04
Morteza Ansarinia <ansarinia@me.com>
2021-03-27 01:38:40 +0100

    NER using spacy rule matching
    Former-commit-id: d58ca8b7718c2b98cc0db383f7651776abe49169 [formerly 3d904359217a017831597c3ffab9b0bd10e3c9e1]
    Former-commit-id: f0d04e64867fd328e5eeac43c3a0a8620d55e54e

---
+++ notebooks/99. NER.py
@@ -0,0 +1,30 @@
+# %% [markdown]
+
+# This notebook demonstrates a method that extracts named entities (e.g., cognitive test names) from a corpus. It will be used during processing and more advanced NLP processing.
+
+
+# %%
+import spacy
+from spacy.matcher import PhraseMatcher
+
+nlp = spacy.load("en_core_web_sm")
+matcher = PhraseMatcher(nlp.vocab, attr='LOWER')
+
+# %%
+
+cognitive_tests = ['digit span', 'stroop', 'n-back']
+patterns = [nlp.make_doc(text) for text in cognitive_tests]
+
+corpus = [
+    'we used Stroop, n-back, and Digit span tasks to measure executive functions skill.',
+    'but we only used TMT and Stroop for cognitive control measurements',
+]
+
+docs = nlp.pipe(corpus)
+
+# %%
+
+for doc in docs:
+    matcher.add('CognitiveTest', patterns)
+    matches = matcher(doc)
+    print(matches)
