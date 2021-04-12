95a3bec4999b6fca099e4660a8693a51cb5d8894
Morteza Ansarinia <ansarinia@me.com>
2021-04-12 04:38:16 +0200

    csv files now contain publications before 2000; missing pub year is filled by other sources.
    Former-commit-id: 28fe4bdb17a6efa5c8bc5e993a7180835dae5341 [formerly 502331a90cb57a633aed582da73ae87516730a98] [formerly 28fe4bdb17a6efa5c8bc5e993a7180835dae5341 [formerly 502331a90cb57a633aed582da73ae87516730a98] [formerly 8552f54407780a0dcb5a95222197cf5bbb36692f [formerly 4871598e9db95d60efe55e699bf874a12ff77e25]]]
    Former-commit-id: 61eabf897272c6edd36f885f1ebe93c85e85920c
    Former-commit-id: 51fb374d68ee1f5b94b700a729e163d7eac63e04
    Former-commit-id: a52d8af3868f3e831c1ff9329e1df8a44a6a4557
    Former-commit-id: eaa9500ae2779c7ab8be86781e28cbb55c7c686f

--- python/cogtext/pubmed.py
+++ python/cogtext/pubmed.py
@@ -3,12 +3,13 @@ import requests
 import xmltodict
 from pathlib import Path
 from collections import OrderedDict 
+from datetime import date
 
 
 NCBI_EUTILS_BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
 
 
-def search_and_store(query, output_file, db='pubmed', api_key=os.environ['NCBI_API_KEY']):
+def search_and_store(query, output_file: Path, db='pubmed', api_key=os.environ['NCBI_API_KEY']):
   """Search for a term and store abstracts in a file
 
   Args:
@@ -31,13 +32,13 @@ def search_and_store(query, output_file, db='pubmed', api_key=os.environ['NCBI_A
       'usehistory': 'y',
       'db': db,
       'retmax': 0,
-      'reldate': 21 * 365,
+      'reldate': (date.today() - date(1900, 1, 1)).days,
       'api_key': api_key
   }
 
   response = requests.get(url, params=params)
   search_response = xmltodict.parse(response.text)
-  results_count = search_response['eSearchResult']['Count']
+  results_count = int(search_response['eSearchResult']['Count'])
 
   print(f'[PubMed] Succesfully stored {results_count} hits on NCBI history server.')
 
@@ -52,21 +53,42 @@ def search_and_store(query, output_file, db='pubmed', api_key=os.environ['NCBI_A
       'retmode': 'xml'
   }
 
-  response = requests.post(url, params)
+  retstart = 0
+  retmax = 10000
 
-  # step 3: store abstracts
-  Path(output_file).parent.mkdir(parents=True, exist_ok=True)
-  with open(output_file, 'w') as f:
-    f.write(response.text)
+  while retstart < results_count:
+    params['retstart'] = retstart
+    response = requests.post(url, params)
+
+    # step 3: store abstracts
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+
+    output_chunk_file = Path(output_file.as_posix().replace('.xml', f'.{retstart}.xml' if retstart > 0 else '.xml'))
+
+    with open(output_chunk_file, 'w') as f:
+      f.write(response.text)
+
+    retstart += retmax
+
   print(f'[PubMed] Succesfully stored hits in {output_file}.')
 
 
 def cleanup_abstract(abstract_text):
     """PubMed returns abstract with semantic tags. This function cleans those tags and keep the text."""
-    select_content = lambda c: c if isinstance(c ,str) else c['#text'] if (c is not None and ('#text' in c)) else ''
+    select_content = lambda c: c if isinstance(c, str) else c['#text'] if (c is not None and ('#text' in c)) else ''
 
     if isinstance(abstract_text, list):
         return ' '.join([select_content(a) for a in abstract_text])
     elif isinstance(abstract_text, OrderedDict):
         return select_content(abstract_text)
     return abstract_text    # when the abstract is string
+
+
+def merge_and_store_xml(output_fname: Path, *args):
+  from xml.etree import ElementTree
+
+  xml_data = ElementTree.parse(args[0]).getroot()
+  data = [ElementTree.parse(f).getroot() for f in args[1:]]
+  [xml_data.extend(d) for d in data]
+  with open('~/combined.xml', 'w'):
+    ElementTree(xml_data).write('', 'utf-8')
