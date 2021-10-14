b697f879dfc597f327aa2a0275019a545cc43830
morteza <ansarinia@me.com>
2021-10-14 18:35:08 +0200

    pretrained sentence-transformer models for pubmed abstracts
    Former-commit-id: e9d5c28d5f171c459a7bc07a800a2fe0acfd71e4 [formerly e37c2e3c645ce3b74faee5f9dac0034a84f671d8]
    Former-commit-id: 227c34d06678ac2610695387c6dcf398814d617a

--- jobs/doc_embedding.py
+++ jobs/doc_embedding.py
@@ -6,17 +6,18 @@ import numpy as np
 
 
 # PARAMETERS
+DATASET_NAME = 'pubmed_abstracts'  # or pubmed_abstracts_preprocessed
 MODEL_NAME = 'all-MiniLM-L6-v2'  # or a faster model: 'paraphrase-MiniLM-L3-v2'
-EMBEDDINGS_FILE = Path(f'models/pubmed_abstracts_{MODEL_NAME}.embeddings')
+EMBEDDINGS_FILE = Path(f'models/{DATASET_NAME}_{MODEL_NAME}.embeddings')
 
 # Initialize models/ folder if does not exist yet.
 Path('models/').mkdir(parents=True, exist_ok=True)
 
 # DATA
-PUBMED = pd.read_csv(
-    'data/pubmed_abstracts_preprocessed.csv.gz'
-).dropna(subset=['abstract']).reset_index()
+PUBMED = pd.read_csv(f'data/{DATASET_NAME}.csv.gz')
 
+# PREP
+PUBMED = PUBMED.dropna(subset=['abstract']).reset_index()
 PUBMED.rename(columns={'subcategory': 'label'}, errors='ignore', inplace=True)
 
 # MODEL FITTING
@@ -28,4 +29,5 @@ embeddings = model.encode(
 
 # STORE RESULTS
 np.savez(EMBEDDINGS_FILE, embeddings)
+
 print('Done!')
