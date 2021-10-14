c7a4ee9192b359f78c423f47d7d3fcf14e1b339b
Morteza Ansarinia <ansarinia@me.com>
2021-10-14 20:54:37 +0200

    add hpc job for doc embedding on cpu
    Former-commit-id: f6687987f7418378d4382675618b1dda6834808c [formerly ecb8585cf96746b5379f98cc43c304158a884cb7]
    Former-commit-id: 3263ccac3dd17f731f4fb5baace89d55827320ec

--- jobs/doc_embedding.py
+++ jobs/doc_embedding.py
@@ -1,4 +1,3 @@
-import argparse
 from pathlib import Path
 from sentence_transformers import SentenceTransformer
 import pandas as pd
@@ -6,28 +5,33 @@ import numpy as np
 
 
 # PARAMETERS
-DATASET_NAME = 'pubmed_abstracts'  # or pubmed_abstracts_preprocessed
+DATASET_NAMES = ['pubmed_abstracts', 'pubmed_abstracts_preprocessed']
 MODEL_NAME = 'all-MiniLM-L6-v2'  # or a faster model: 'paraphrase-MiniLM-L3-v2'
-EMBEDDINGS_FILE = Path(f'models/{DATASET_NAME}_{MODEL_NAME}.embeddings')
+DEVICE = 'cpu'  # or 'gpu'
 
-# Initialize models/ folder if does not exist yet.
-Path('models/').mkdir(parents=True, exist_ok=True)
 
-# DATA
-PUBMED = pd.read_csv(f'data/{DATASET_NAME}.csv.gz')
+for dataset_name in DATASET_NAMES:
+    print(f'Encoding {dataset_name}...')
+    EMBEDDINGS_FILE = Path(f'models/{dataset_name}_{MODEL_NAME}_{DEVICE}.embeddings')
 
-# PREP
-PUBMED = PUBMED.dropna(subset=['abstract']).reset_index()
-PUBMED.rename(columns={'subcategory': 'label'}, errors='ignore', inplace=True)
+    # Initialize models/ folder if does not exist yet.
+    Path('models/').mkdir(parents=True, exist_ok=True)
 
-# MODEL FITTING
-model = SentenceTransformer(MODEL_NAME)
-embeddings = model.encode(
-    PUBMED['abstract'].to_list(),
-    normalize_embeddings=True,
-    show_progress_bar=True)
+    # DATA
+    data = pd.read_csv(f'data/{dataset_name}.csv.gz')
 
-# STORE RESULTS
-np.savez(EMBEDDINGS_FILE, embeddings)
+    # PREP
+    data = data.dropna(subset=['abstract']).reset_index()
+    data.rename(columns={'subcategory': 'label'}, errors='ignore', inplace=True)
 
-print('Done!')
+    # MODEL FITTING
+    model = SentenceTransformer(MODEL_NAME)
+    embeddings = model.encode(
+        data['abstract'].to_list(),
+        normalize_embeddings=True,
+        show_progress_bar=True)
+
+    # STORE RESULTS
+    np.savez(EMBEDDINGS_FILE, embeddings)
+
+    print('Done!')
