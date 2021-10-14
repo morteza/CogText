27fff9ccb411ee1dec5df7cc781107ba96c4a200
Morteza Ansarinia <ansarinia@me.com>
2021-10-14 20:54:37 +0200

    add hpc job for doc embedding on cpu
    Former-commit-id: f6687987f7418378d4382675618b1dda6834808c [formerly ecb8585cf96746b5379f98cc43c304158a884cb7]
    Former-commit-id: 3263ccac3dd17f731f4fb5baace89d55827320ec

---
+++ jobs/doc_embedding.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#SBATCH --job-name=cogtext_doc_embedding
+#SBATCH --chdir=/work/projects/acnets/repositories/
+#SBATCH --output=/work/projects/acnets/logs/%x_%A.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32gb
+#SBATCH --partition=batch
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=morteza.ansarinia@uni.lu
+
+
+echo "SLURM_JOB_ID: " $SLURM_JOB_ID
+
+
+# enable access to the `module` and install conda.
+[ -f /etc/profile ] && source /etc/profile
+module purge
+module load lang/Anaconda3
+
+# purge old stuff (conda env, codes, data, outputs, etc)
+conda deactivate
+conda env remove -n cogtext
+
+if [ ! -d "cogtext/" ]
+then 
+  git clone --recurse-submodules ssh://git@gitlab.uni.lu:8022/xcit/efo/cognitive-tests-text-analysis.git cogtext/
+  cd cogtext/
+else
+  cd cogtext/
+  git reset --hard
+  git clean -xdf
+  git pull --recurse-submodules
+fi
+
+
+# create and prepare a new conda environment (also installs git and git-lfs via conda)
+conda create -n cogtext python=3.9 --yes
+source activate cogtext
+conda install -c conda-forge git git-lfs
+
+# install dependencies and ipython to run the notbooks
+pip install pip -U
+pip install -r requirements_hpc.txt
+# pip install ipython jupyter
+
+# Fix transformers bug when nprocess > 1
+# export TOKENIZERS_PARALLELISM=false
+
+# run the code
+python jobs/doc_embedding.py
+
+# push the BIDS changes back to the gitlab repository
+git add -A .
+git commit -m "CI/HPC/doc_embedding.sh auto-commit (SLURM_JOB_ID: ${SLURM_JOB_ID})"
+git push origin dev
+
+# That's it!
