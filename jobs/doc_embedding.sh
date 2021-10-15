8d46e23fa1593f2c300b5976e0a091aad1c166af
Morteza Ansarinia <ansarinia@me.com>
2021-10-15 11:31:49 +0200

    install cuda on hpc
    Former-commit-id: d54851bace541b61ad3242daadb94f0c04329710

--- jobs/doc_embedding.sh
+++ jobs/doc_embedding.sh
@@ -38,8 +38,10 @@ fi
 
 # create and prepare a new conda environment (also installs git and git-lfs via conda)
 conda create -n cogtext python=3.9 --yes
-source activate cogtext
+conda activate cogtext
+conda update conda
 conda install -c conda-forge git git-lfs
+conda install -c conda-forge cudatoolkit
 
 # install dependencies and ipython to run the notbooks
 pip install pip -U
