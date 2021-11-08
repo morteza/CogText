16752290f6233538890b15587bbe1a9a2a6339c9
Morteza Ansarinia <ansarinia@me.com>
2021-11-08 22:12:31 +0100

    refactor single-topic analysis codes and add partial correlation plot
    Former-commit-id: ade6c74cb9f139119080fe83854f7c238eb7dd58

---
+++ python/cogtext/clusterers.py
@@ -0,0 +1,20 @@
+def fit_hdbscan(X, y=None):
+  """Cluster documents with HDBSCAN (output: cluster labels, membership scores)
+  """
+
+  from hdbscan import HDBSCAN
+  import joblib
+
+  joblib_cache = joblib.Memory(location='tmp')
+
+  clusterer = HDBSCAN(min_cluster_size=10,
+                      min_samples=1,
+                      metric='euclidean',
+                      core_dist_n_jobs=1,
+                      memory=joblib_cache,
+                      prediction_data=True)
+
+  clusters = clusterer.fit_predict(X)
+  scores = clusterer.probabilities_
+
+  return clusters, scores
