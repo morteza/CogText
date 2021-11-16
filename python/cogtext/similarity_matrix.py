7e4729481ac006799e557950f31198a036534d24
Morteza Ansarinia <ansarinia@me.com>
2021-11-16 14:59:40 +0100

    refactor similarity matrix
    Former-commit-id: caff129260f4834ecea83f449fcc4dab050a07ab

---
+++ python/cogtext/similarity_matrix.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def similarity_matrix(H, metric='cosine', pivot_by_category=True):
+  assert metric in ['cosine'], 'Invalid similarity metric'
+
+  H_sim = pd.DataFrame(cosine_similarity(H),
+                       index=H.index.get_level_values('label'),
+                       columns=H.index.get_level_values('label'))
+
+  if pivot_by_category:
+    tasks = H.query('index.get_level_values("category") == "CognitiveTask"'
+                    ).index.get_level_values('label').unique()
+    constructs = H.query('index.get_level_values("category") == "CognitiveConstruct"'
+                         ).index.get_level_values('label').unique()
+    categorized_df = H_sim.drop(index=tasks, columns=constructs)
+    return categorized_df
+
+  return H_sim
