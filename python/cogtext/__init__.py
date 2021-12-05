9eb1c9e6257cf9cc71f53178d7eb42f974604b60
Morteza Ansarinia <ansarinia@me.com>
2021-12-05 12:45:55 +0100

    add "versioning watermarks" to the notebooks for reproducibility
    Former-commit-id: bc1c42671cea1685475c2460d0520ce96564f3f0

--- python/cogtext/__init__.py
+++ python/cogtext/__init__.py
@@ -2,3 +2,7 @@ from .datasets.pubmed import *
 from .co_occurrence import co_occurrence_matrix
 
 from .svd_embedding import svd_embedding
+
+from datetime import datetime
+
+__version__ = '0.1.' + datetime.now().strftime('%Y%m%d%H')
