7be00b6c95e001eaeaf606b62f60b7400213c92a
Morteza Ansarinia <ansarinia@me.com>
2022-02-01 14:08:52 +0100

    update figure for cogsci2022
    Former-commit-id: b82965f7f30b74d98b5bcb28604a8a42f1f8564d

--- python/cogtext/plotting/rose_plot.py
+++ python/cogtext/plotting/rose_plot.py
@@ -8,10 +8,26 @@ def rose_plot(data: pd.DataFrame, ax=None, **kwargs):
   if ax is None:
     ax = plt.subplot(polar=True)
 
-  polar_ticks = np.linspace(0., 2 * np.pi, data['task'].nunique() + 1)
-  ax.set_xticks(polar_ticks[:-1])
-  xticks_labels = [d[:5] for d in data['task'].to_list()]  # shorten task labels
-  ax.set_xticklabels(xticks_labels, fontsize=kwargs.get('fontsize', 12))
+  ax.set_rmax(2)
+
+  polar_ticks = np.linspace(0., -2 * np.pi, data['task'].nunique())
+  ax.set_xticks(polar_ticks)
+  xticks_labels = [d[:4] for d in data['task'].to_list()]  # shorten task labels
+  xticks_labels[-1] = ''
+  ax.set_xticklabels(xticks_labels)
+
+  label_angles = np.linspace(0, 2 * np.pi, len(xticks_labels)) + (np.pi / 2)
+  label_angles[np.cos(label_angles) < 0] = label_angles[np.cos(label_angles) < 0] + np.pi
+  label_angles = np.rad2deg(label_angles)
+
+  for label, angle in zip(ax.get_xticklabels(), label_angles):
+      x, y = label.get_position()
+      lab = ax.text(x, y+.05, label.get_text(), transform=label.get_transform(),
+                    fontsize=kwargs.get('fontsize', 8),
+                    color='gray',
+                    ha=label.get_ha(), va=label.get_va())
+      lab.set_rotation(angle)
+  ax.set_xticklabels([])
 
   # If you want the first axis to be on top:
   ax.set_theta_offset(np.pi / 2)
@@ -24,6 +40,7 @@ def rose_plot(data: pd.DataFrame, ax=None, **kwargs):
   bar_width = 2 * np.pi / len(data)
   bar_x = [i * bar_width for i in data.index]
 
+
   ax.set_ylim(0, data.drop(columns=['task', 'index'], errors='ignore').max().max() + 0.001)
 
   for col in data.columns:
@@ -35,4 +52,4 @@ def rose_plot(data: pd.DataFrame, ax=None, **kwargs):
         data[col],
         edgecolor='white',
         label=col,
-        alpha=kwargs.get('alpha', 0.5))
+        alpha=kwargs.get('alpha', 0.3))
