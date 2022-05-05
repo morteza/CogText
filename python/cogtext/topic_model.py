8ad9be43dd34a71fe6e77254c2d1cf8961d96cab
Morteza Ansarinia <ansarinia@me.com>
2022-05-06 00:21:04 +0200

    update embeddings notebooks + store all the embeddings in a single file

--- python/cogtext/topic_model.py
+++ python/cogtext/topic_model.py
@@ -2,13 +2,14 @@ import hdbscan
 from hdbscan import HDBSCAN
 import joblib
 import numpy as np
-from pathlib import Path
 
 
 class TopicModel():
   def __init__(self, parametric_umap=True, verbose=False) -> None:
 
     self.verbose = verbose
+    self.parametric_umap = parametric_umap
+    self.umap_embeddings_ = None
 
     # importing UMAP takes a while, so we import only when retraining is needed
     if parametric_umap:
@@ -16,46 +17,43 @@ class TopicModel():
     else:
       from umap import UMAP
 
-    self.reducer = UMAP(n_neighbors=15,
-                        n_components=5,
-                        metric='euclidean',
-                        min_dist=0.0,
-                        verbose=True,
-                        n_jobs=-1)
+    self.reducer_ = UMAP(n_neighbors=15,
+                         n_components=5,
+                         metric='euclidean',
+                         min_dist=0.0,
+                         verbose=self.verbose,
+                         n_jobs=-1)
 
-    self.clusterer = HDBSCAN(min_cluster_size=100,
-                             min_samples=1,
-                             metric='euclidean',
-                             core_dist_n_jobs=-1,
-                             memory=joblib.Memory(location='tmp'),
-                             prediction_data=True)
+    self.clusterer_ = HDBSCAN(min_cluster_size=100,
+                              min_samples=1,
+                              metric='euclidean',
+                              core_dist_n_jobs=-1,
+                              memory=joblib.Memory(location='tmp'),
+                              prediction_data=True)
 
   def fit_transform(self, X, y=None, umap_embeddings=None):
-    """First reduce dimensionality of the input and apply soft-clustering.
-    
+    """Reduce the dimensionality of the input and then apply soft-clustering.
+
     Returns:
-      clusters: array of cluster labels.
-      weights: array of cluster weights (soft clusters where applying argmax equals
-        `clusters`).
+      clusters: array of assigned cluster labels to each data point.
+      weights: array of cluster membership weights (soft clusters where applying argmax
+               equals `clusters`).
     """
 
-    if umap_embeddings is None:
-      reduced = self.reducer.fit_transform(X, y)
-      reduced = np.nan_to_num(reduced)
-    else:
-      # load the embedding from cached file
-      reduced = umap_embeddings
+    self.umap_embeddings_ = umap_embeddings
 
-    self.verbose and print('[TopicModel] Reduced embedding dimension. Now clustering...')
+    if self.umap_embeddings_ is None:
+      self.umap_embeddings_ = self.reducer_.fit_transform(X, y)
+      self.umap_embeddings_ = np.nan_to_num(self.umap_embeddings_)
+      self.verbose and print('[TopicModel] Reduced embeddings dimension. Now clustering...')
 
-    clusters = self.clusterer.fit_predict(reduced)
-
-    self.verbose and print('[TopicModel] Clustered embedding. Now computing weights...')
+    clusters = self.clusterer_.fit_predict(self.umap_embeddings_)
+    self.verbose and print('[TopicModel] Clustered embeddings. Now computing membership weights...')
 
     # weights = self.clusterer.probabilities_
-    weights = hdbscan.all_points_membership_vectors(self.clusterer)
+    weights = hdbscan.all_points_membership_vectors(self.clusterer_)
 
-    # commented because of soft clustering; we don't need to zero out weights for the noise points
+    # commented because of soft clustering; we don't want to zero out weights for noisy data
     # weights[clusters < 0] = 0.0
 
     self.verbose and print('[TopicModel] Done!')
